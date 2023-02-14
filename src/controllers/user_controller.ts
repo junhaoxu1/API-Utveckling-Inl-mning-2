deba09bf80227e773078b94cb29d9433aa7cefd6
Andreas Xu <junhaoxu123@gmail.com>
2023-02-14 14:16:53 +0100

    Added login function

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -31,11 +31,39 @@ export const login = async (req: Request, res: Response) => {
     sub: user.id,
     first_name: user.first_name,
     email: user.email,
+  };
+
+  if (!process.env.ACCESS_TOKEN_SECRET) {
+    return res.status(500).send({
+      status: "error",
+      message: "No access token secret defined",
+    });
   }
+
+  const access_token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: process.env.ACCESS_TOKEN_LIFETIME || "4h",
+  });
+
+  if (!process.env.REFRESH_TOKEN_SECRET) {
+    return res.status(500).send({
+      status: "Error",
+      message: "No refresh token secret defined",
+    });
+  }
+
+  const refresh_token = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_LIFETIME || "1d",
+  });
+  res.send({
+    status: "success",
+    data: {
+      access_token, // access_token: access_token
+      refresh_token,
+    },
+  });
+};
+export const register = async (req: Request, res: Response) => {
+  
 };
 
-export const show = async (req: Request, res: Response) => {};
-
 export const store = async (req: Request, res: Response) => {};
-
-export const update = async (req: Request, res: Response) => {};
