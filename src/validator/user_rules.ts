54fdfb4aba112ce30916c35e65d23f1b5d850a97
junhaoxu1 <97689187+junhaoxu1@users.noreply.github.com>
2023-02-18 14:03:09 +0100

    Validator for last_name

--- src/validator/user_rules.ts
+++ src/validator/user_rules.ts
@@ -6,6 +6,7 @@ import { getUserByEmail } from '../services/user_service'
 
 export const createUserRules = [
 	body('first_name').isString().bail().isLength({ min: 3 }),
+	body('last_name').isString().bail().isLength({ min: 1 }),
 	body('email').isEmail().custom(async (value: string) => {
 		const user = await getUserByEmail(value)
 
